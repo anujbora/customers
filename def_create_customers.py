6a5e0930fa6a8aef8395f0146e73c98a8bd5f69e
KehanWu <kehan_wu@outlook.com>
2017-02-21 23:10:02 -0500

    create_customers

---
+++ def_create_customers.py
@@ -0,0 +1,40 @@
+
+customers = 
+    {
+        'id': 1,
+        'first-name': 'fido',
+        'last-name': 'hanks'
+        'gender': 'male',
+        'age': 23,
+        'email': 'fido1@gmail.com',
+        'address-line1':'10420 Queens Blvd',
+        'address-line2':'16-V',
+        'phonenumber' : 123456,      
+    },
+    {
+        'id': 2,
+        'first-name': 'shirley',
+        'last-name': 'yang'
+        'gender': 'female',
+        'age': 22,
+        'email': 'shirley2@gmail.com',
+        'address-line1':'8th street Mahaton',
+        'address-line2':'302',
+        'phonenumber' : 234567,   
+    }
+]
+
+@app.route('/customers', methods=['POST'])
+def create_customers():
+    payload = request.get_json()
+    if is_valid(payload):
+        id = next_index()
+        customer = {'id': id, 'first-name': payload['first-name'], 'last-name': payload['last-name'], 'gender': payload['gender'], 'age': payload['age'],'email':payload['email'], 'address-line1': payload['address-line1'], 'address-line2': payload['address-line2'], 'phonenumber': payload['phonenumber']}
+        customers.append(customer)
+        message = customer
+        rc = HTTP_201_CREATED
+    else:
+        message = { 'error' : 'Data is not valid' }
+        rc = HTTP_400_BAD_REQUEST
+
+    return make_response(jsonify(message), rc)
